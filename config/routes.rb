302e9f20417c5db7bdd6d1744e3141ba484c1101
Daniel Stara <dstara12@gmail.com>
2023-02-11 23:00:51 -0600

    login ability

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,9 @@ Rails.application.routes.draw do
   get "about", to: "about#index"
   get "contact", to: "contact#index"
   
+  get "sign_up", to: "registrations#new"
+  post "sign_up", to: "registrations#create"
+  
   # Defines the root path route ("/")
   # root "articles#index"
 end
