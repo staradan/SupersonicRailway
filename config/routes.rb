625591170453648129f34eb294d2ac8700e0f0d8
Daniel Stara <dstara12@gmail.com>
2023-02-14 20:57:30 -0600

    cleanup_stuff

--- config/routes.rb
+++ config/routes.rb
@@ -5,9 +5,16 @@ Rails.application.routes.draw do
   root to: "main#index"
   get "about", to: "about#index"
   get "contact", to: "contact#index"
+
+  get "password", to: "passwords#edit", as: :edit_password
+  patch "password", to: "passwords#update"
   
   get "sign_up", to: "registrations#new"
   post "sign_up", to: "registrations#create"
+
+  get "sign_in", to: "sessions#new"
+  post "sign_in", to: "sessions#create"
+  delete "logout", to: "sessions#destroy"
   
   # Defines the root path route ("/")
   # root "articles#index"
